9308b8e7fb3c3f893276fe4cd71890f857cf4f18
Brandon Hadler <brandon.hadler30@gmail.com>
2019-07-17 10:35:31 -0700

    test fix home page

--- src/client/components/presentational/Mission.js
+++ src/client/components/presentational/Mission.js
@@ -2,20 +2,22 @@ import React from "react";
 
 const Mission = () => {
   return (
-    <div className="mission-container">
-      <p className="mission-text-one">
-        Helping others make the most of their life.
-      </p>
-      <p>
-        Taking your first step towards self improvement can be difficult and at
-        times overwhelming. We all have priorities in our life where we spend
-        too much, or too little time. Our lives are shaped by habits and
-        routines, some more obvious than others.
-      </p>
-      <p>
-        Vantage Point was created to help identify opportunites for self
-        improvement in your life.
-      </p>
+    <div>
+      <div className="mission-container">
+        <p className="mission-text-one">
+          Helping others make the most of their life.
+        </p>
+        <p>
+          Taking your first step towards self improvement can be difficult and
+          at times overwhelming. We all have priorities in our life where we
+          spend too much, or too little time. Our lives are shaped by habits and
+          routines, some more obvious than others.
+        </p>
+        <p>
+          Vantage Point was created to help identify opportunites for self
+          improvement in your life.
+        </p>
+      </div>
     </div>
   );
 };
